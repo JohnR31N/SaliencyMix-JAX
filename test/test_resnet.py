bcd19ec968f11c3cb15ae862a0d93d29679712ad
John Zhang <bluesoulreborn@gmail.com>
2026-05-22 17:24:37 +0800

    Update test_resnet.py

--- test/test_resnet.py
+++ test/test_resnet.py
@@ -1,23 +1,22 @@
 import jax
 import jax.numpy as jnp
 
-from model.resnet import ResNet18
+from models.resnet import ResNet18
 
 
 key = jax.random.PRNGKey(0)
 
 model = ResNet18(num_classes=10)
-
 x = jnp.ones((8, 32, 32, 3))
 
 variables = model.init(key, x, train=True)
 
-logits = model.apply(
+logits, updated_state = model.apply(
     variables,
     x,
     train=True,
     mutable=["batch_stats"],
 )
 
-print(type(logits))
-print(logits)
+print("logits shape:", logits.shape)
+print("updated_state keys:", updated_state.keys())
